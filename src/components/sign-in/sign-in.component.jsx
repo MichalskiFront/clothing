fcdd7cf5b2e3f4c4de8749589b462d05ce168717
Kamil Michalski <kamilpro150@gmail.com>
2020-04-08 18:57:43 +0200

    reduxConfig - added redux - implemented userReducer and userActions to header and app components

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -6,6 +6,7 @@ import CustomButton from '../custom-button/custom-button.component';
 import { signInWithGoogle } from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
+import { auth } from 'firebase';
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -17,8 +18,16 @@ class SignIn extends React.Component {
     };
   }
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
+
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+    } catch (error) {
+      console.log(error);
+    }
 
     this.setState({ email: '', password: '' });
   };
